163ef7fc2753620c80392b6a8533caac1168f23c
Vanessa Su <vnessa.su@gmail.com>
2021-04-23 13:40:05 -0400

    Add convertAmountTo method

--- __tests__/exchange-rate-api.test.js
+++ __tests__/exchange-rate-api.test.js
@@ -3,20 +3,21 @@ import ExchangeRateApi from './../src/js/exchange-rate-api.js';
 
 describe('ExchangeRateApi static methods', () => {
   it('should return a promise given valid inputs', async () => {
-    global.fetch = jest.fn().mockImplementationOnce(() => {
-      Promise.resolve({
-        json: () => Promise.resolve({
-          "result": "success",
-          "base_code": "EUR",
-          "target_code": "GBP",
-          "conversion_rate": 0.8412,
-          "conversion_result": 5.8884
-        }),
-      })
-    });
-    fetch.mockClear();
+    // global.fetch = jest.fn().mockImplementationOnce(() => {
+    //   Promise.resolve({
+    //     json: () => Promise.resolve({
+    //       "result": "success",
+    //       "base_code": "EUR",
+    //       "target_code": "GBP",
+    //       "conversion_rate": 0.8412,
+    //       "conversion_result": 5.8884
+    //     }),
+    //   })
+    // });
+    // fetch.mockClear();
     let exchangeRatePromise = ExchangeRateApi.convertAmountTo("EUR", "GBP", 7);
     let data;
+    console.log(exchangeRatePromise);
     exchangeRatePromise.then(function(response){data = response;});
     expect(fetch).toHaveBeenCalledTimes(1);
     exepect(data.result).toBe("success");
--- src/js/exchange-rate-api.js
+++ src/js/exchange-rate-api.js
@@ -1,5 +1,15 @@
 export default class ExchangeRateApi{
   static convertAmountTo(originalCurrency, targetCurrency, amount){
-    return new Promise();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${originalCurrency}/${targetCurrency}/${amount}`;
+    return fetch(url)
+      .then(function (response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return Error(error);
+      });
   }
 }
