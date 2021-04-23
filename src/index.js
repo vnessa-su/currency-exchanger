19ae6a95e2be8b35756cdd4f9b6e320b5fb6808d
Vanessa Su <vnessa.su@gmail.com>
2021-04-23 16:20:14 -0400

    Add populateCurrencies function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import $ from 'jquery';
 import ExchangeRateApi from './js/exchange-rate-api.js';
 
 $(document).ready(function(){
+  populateCurrencies();
   $("#submitAmountButton").click(function(){
     const amount = parseFloat($("#amountInput").val());
     let exchangeRatePromise = ExchangeRateApi.convertAmountTo("EUR", "GBP", amount);
@@ -20,3 +21,24 @@ $(document).ready(function(){
     });
   });
 });
+
+function populateCurrencies(){
+  const currencies = [
+    ["USD", "United States Dollar", "&#36;"], 
+    ["EUR", "Euro - European Union", "&#8364;"], 
+    ["GBP", "Pound Sterling - United Kingdom", "&#163;"], 
+    ["CNY", "Chinese Renminbi", "&#20803;"], 
+    ["JPY", "Japanese Yen", "&#165;"], 
+    ["KRW", "South Korean Won", "&#8361;"], 
+    ["AUD", "Australian Dollar", "&#36;"]
+  ];
+
+  currencies.forEach(element => {
+    const currencyCode = element[0];
+    const currencyTitle = element[1];
+    // const currencySymbol = element[2];
+    const htmlString = `<option value="${currencyCode} title="${currencyTitle}">${currencyCode}</option>`;
+    $("#originalCurrencySelect").append(htmlString);
+    $("#targetCurrencySelect").append(htmlString);
+  });
+}
