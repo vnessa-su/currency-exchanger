f895f9cb53dd4838a4fe84551ede8e908c6809d8
Vanessa Su <vnessa.su@gmail.com>
2021-04-23 18:13:07 -0400

    Add error handling method

--- src/js/exchange-rate-api.js
+++ src/js/exchange-rate-api.js
@@ -3,13 +3,36 @@ export default class ExchangeRateApi{
     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${originalCurrency}/${targetCurrency}/${amount}`;
     return fetch(url)
       .then(function(response){
-        if(!response.ok){
-          throw Error(response.statusText);
-        }
-        return response.json();
+        const dataFromResponse = this.checkForResponseErrors(response);
+        return dataFromResponse;
       })
       .catch(function(error){
         return Error(error);
       });
+  }
+
+  static checkForResponseErrors(response){
+    const data = response.json();
+    if(!response.ok){
+      throw Error(response.statusText);
+    }
+    if(data.result === "error"){
+      const errorType = data["error-type"];
+      switch(errorType){
+        case "unsupported-code":
+          throw Error("Unsupported Currency Code");
+        case "malformed-request":
+          throw Error("Malformed Request URL");
+        case "invalid-key":
+          throw Error("API Key is Invalid");
+        case "inactive-account":
+          throw Error("Inactive Account");
+        case "quota-reached":
+          throw Error("Request Limit Reached");
+        default:
+          throw Error("Unknown Error");
+      }
+    }
+    return data;
   }
 }
