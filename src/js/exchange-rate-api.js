0a6676383f16d5074c88ad1a027c80e98ae31a5d
Vanessa Su <vnessa.su@gmail.com>
2021-04-25 09:00:18 -0400

    Add getAllConversionRates method

--- src/js/exchange-rate-api.js
+++ src/js/exchange-rate-api.js
@@ -26,6 +26,20 @@ export default class ExchangeRateApi{
         return Error(error);
       });
   }
+
+  static getAllConversionRates(targetCurrency){
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${targetCurrency}`;
+    return fetch(url)
+      .then(function(response){
+        if(!response.ok){
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error){
+        return Error(error);
+      });
+  }
   
   static checkForResponseError(data){
     if(data.result === "error"){
